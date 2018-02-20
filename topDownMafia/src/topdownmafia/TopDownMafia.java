411f58e90dfdd3cc8950026583cedb032bef8e39
majoh <majoh@Mathias-Pc>
2018-02-20 23:22:34 +0100

    Initial project from Netbeans

---
+++ topDownMafia/src/topdownmafia/TopDownMafia.java
@@ -0,0 +1,16 @@
+package topdownmafia;
+
+/**
+ *
+ * @author majoh
+ */
+public class TopDownMafia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
