914ec9f577b0e26930466d92d74d8b40c37bb4ef
majoh <majoh@Mathias-Pc>
2018-03-10 11:06:46 +0100

    Added the "defaultShader" variable for testing

--- topDownMafia/src/topdownmafia/TopDownMafia.java
+++ topDownMafia/src/topdownmafia/TopDownMafia.java
@@ -6,17 +6,22 @@ import java.util.logging.Logger;
 import util.glWindow;
 import util.Sprite;
 import util.Texture;
+import util.glShaderProgram;
 
 public class TopDownMafia {
     private static final GameLoop GAMELOOP = new GameLoop();
+    public static glShaderProgram defaultShader;
     public static Sprite s;
     
     public static void main(String[] args) throws IOException{        
         glWindow.setWindowSize(1280, 720);
         glWindow.init();
         
+        defaultShader = new glShaderProgram("Assets/shaders/shader.vert", "Assets/shaders/shader.frag");
+        
+        
         try {
-            s = new Sprite(new Texture("D:\\Mathias\\Documents\\NetBeansProjects\\topDownMafia\\topDownMafia\\Assets\\anim.png", 2));
+            s = new Sprite(new Texture("Assets/anim.png", 2));
         } catch (IOException ex) {
             Logger.getLogger(TopDownMafia.class.getName()).log(Level.SEVERE, null, ex);
         }
